06564a15de7e1bb9435790343eaaf60f1888b768
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-08 11:16:20 +0900

    specify type explicitly

--- app/src/main/java/jp/cordea/daggertestappcomponentdemo/AppModule.kt
+++ app/src/main/java/jp/cordea/daggertestappcomponentdemo/AppModule.kt
@@ -1,11 +1,12 @@
 package jp.cordea.daggertestappcomponentdemo
 
 import android.app.Application
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 
 @Module
 class AppModule {
     @Provides
-    fun provideContext(application: Application) = application.applicationContext
+    fun provideContext(application: Application): Context = application.applicationContext
 }
